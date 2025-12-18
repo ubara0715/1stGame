54d062a359712cb5546ddcf5133e0b5e99eaca10
Ubara0715 <c24028@std.it-college.ac.jp>
2025-12-18 11:32:44 +0900

    using整えた

--- Assets/Scripts/All/Animations.cs
+++ Assets/Scripts/All/Animations.cs
@@ -1,11 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.Video;
 
 public class Animations : MonoBehaviour
 {
